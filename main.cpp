874ba6363471d9ebab82310bc62cc4f645b2c426
szaszkarina <szaszkarina11@gmail.com>
2018-05-16 16:41:57 +0300

    Am adaugat arbori.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,46 @@
 
 using namespace std;
 
-int numarAnimal = 1;
+int numarAnimal = 1;//CODUL DE INREGISTRARE AL CAINELUI
+
+struct data
+{
+    int zi;
+    int luna;
+    int an;
+
+    void print()
+    {
+        cout<<zi<<"."<<luna<<"."<<an;
+    }
+};
+
+struct nodProgramare //ARBORE BINAR, FIU STANGA SI FIU DREAPTA
+{
+    int numarAnimal;
+
+    data dataInceput;
+    data dataSfarsit;
+
+    nodProgramare *fiuStanga;
+    nodProgramare *fiuDreapta;
+
+    void print()
+    {
+        dataInceput.print();
+        cout<<" - ";
+        dataSfarsit.print();
+        cout<<" animalul " << numarAnimal;
+    }
+};
 
 struct nodPadoc
 {
     int numar;
-    int numarAnimal;
-    bool ocupat;
 
     nodPadoc *urmator;
+
+    nodProgramare *programPadoc;
 };
 
 struct nodAnimal
@@ -20,6 +51,7 @@ struct nodAnimal
     int inaltime;
     int kg;
     int grMancareZi;
+    char rasa[60];
 
     nodAnimal *urmator;
 };
@@ -32,8 +64,7 @@ void adaugarePadoc(nodPadoc *&lista, int numarPadoc)
     {
         lista = new nodPadoc;
         lista->numar = numarPadoc;
-        lista->numarAnimal = -1;
-        lista->ocupat = false;
+        lista->programPadoc = NULL;
         lista->urmator = NULL;
     }
     else
@@ -46,9 +77,19 @@ void adaugarePadoc(nodPadoc *&lista, int numarPadoc)
         aux->urmator = new nodPadoc;
         aux = aux->urmator;
         aux->numar = numarPadoc;
-        aux->numarAnimal = -1;
-        aux->ocupat = false;
+        aux->programPadoc = NULL;
         aux->urmator = NULL;
+    }
+}
+
+void afisareProgramPadoc(nodProgramare *program)
+{
+    if(program != NULL)
+    {
+        afisareProgramPadoc(program->fiuStanga);
+        program->print();
+        cout<<endl;
+        afisareProgramPadoc(program->fiuDreapta); //AFISEZ CE E PE STANGA APOI RADACINA SI APOI DREAPA-INORDINE
     }
 }
 
@@ -60,18 +101,97 @@ void afisareListaPadocuri(nodPadoc *lista)
 
     while(aux != NULL)
     {
-        cout<<"Padoc " << aux->numar << " ocupat " << aux->ocupat<<" animal " << aux->numarAnimal<<endl;
+        cout<<"Padocul " << aux->numar<<" ocupat in: "<<endl;
+        afisareProgramPadoc(aux->programPadoc);
         aux = aux->urmator;
     }
 }
 
-nodPadoc* cautarePadocDisponibil(nodPadoc *lista)
+// 0 - date egale; 1 - data1 < data2; (-1) - data1 > data2
+int comparareDate(data data1, data data2)
 {
-    nodPadoc *aux = lista;
+    if(data1.zi == data2.zi && data1.luna == data2.luna && data1.an == data2.an)
+        return 0;
 
-    while(aux != NULL)
+    if(data1.an < data2.an)
+        return 1;
+    else if(data1.an > data2.an)
+        return -1;
+    else
     {
-        if(aux->ocupat == false)
+        if(data1.luna < data2.luna)
+            return 1;
+        else if(data1.luna > data2.luna)
+            return -1;
+        else
+        {
+            if(data1.zi < data2.zi)
+                return 1;
+            else if(data1.zi > data2.zi)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
+//FUNCTIE CE COMPARA DACA AVEM DOUA DATE LA FEL
+
+bool verificarePadocDisponibil(nodProgramare *program, data dataInceput, data dataSfarsit) //PRIMESTE RADACINA ARBORELUI
+{
+    if(program == NULL)
+        return true;
+
+    if(comparareDate(dataSfarsit, program->dataInceput) == 1)
+        return verificarePadocDisponibil(program->fiuStanga, dataInceput, dataSfarsit);
+    else if(comparareDate(dataInceput, program->dataSfarsit) == -1)
+        return verificarePadocDisponibil(program->fiuDreapta, dataInceput, dataSfarsit);
+    else
+        return false;
+}
+
+void adaugareProgramarePadoc(nodProgramare *&program, nodProgramare *programareNoua)
+{
+    if(program == NULL)
+        program = programareNoua;
+    else
+    {
+        nodProgramare *aux = program;
+
+        while(true)
+        {
+            //daca dataSfarsit<dataInceput mergi pe stanga
+            if(comparareDate(programareNoua->dataSfarsit, aux->dataInceput) == 1)
+            {
+                if(aux->fiuStanga == NULL)
+                {
+                    aux->fiuStanga = programareNoua;
+                    break;
+                }
+                else
+                    aux = aux->fiuStanga;
+            }
+            //daca dataInceput>dataSfarsit mergi pe drepata(comparare in X)
+            else if(comparareDate(programareNoua->dataInceput, program->dataSfarsit) == -1)
+            {
+                if(aux->fiuDreapta == NULL)
+                {
+                    aux->fiuDreapta = programareNoua;
+                    break;
+                }
+                else
+                    aux = aux->fiuDreapta;
+            }
+        }
+    }
+}
+
+nodPadoc* cautarePadocDisponibil(nodPadoc *lista, data dataInceput, data dataSfarsit)
+{
+    nodPadoc *aux = lista;//aux e nodPadoc in lista si contine un arbore
+
+    while(aux != NULL)//parcurgere lista padocuri
+    {
+        if(verificarePadocDisponibil(aux->programPadoc, dataInceput, dataSfarsit) == true)//acum i se atribuie arborele
             return aux;
 
         aux = aux->urmator;
@@ -97,9 +217,27 @@ void adaugareAnimal(nodAnimal *&lista, nodAnimal *animalNou)
 
 void citireAnimalNou(nodAnimal *&listaAnimale, nodPadoc *listaPadocuri)
 {
-    nodPadoc *padocDispobibil;
+    nodPadoc *padocDispobibil;// tot un nod
+    data *dataInceput, *dataFinal;
 
-    padocDispobibil = cautarePadocDisponibil(listaPadocuri);
+    dataInceput = new data;
+    cout<<"Incepand din data:"<<endl<<"zi: ";
+    cin>>dataInceput->zi;
+    cout<<"luna: ";
+    cin>>dataInceput->luna;
+    cout<<"an: ";
+    cin>>dataInceput->an;
+
+    dataFinal = new data;
+    cout<<"Pana in data:"<<endl<<"zi: ";
+    cin>>dataFinal->zi;
+    cout<<"luna: ";
+    cin>>dataFinal->luna;
+    cout<<"an: ";
+    cin>>dataFinal->an;
+
+    padocDispobibil = cautarePadocDisponibil(listaPadocuri, *dataInceput, *dataFinal);// Daca exista padoc disponibil va avea adresa acelui nod
+                                                                                        //Daca nu exista padoc disponibil e null
 
     if(padocDispobibil != NULL)
     {
@@ -116,17 +254,24 @@ void citireAnimalNou(nodAnimal *&listaAnimale, nodPadoc *listaPadocuri)
         cin>>animalNou->inaltime;
         cout<<"Gr mancare / zi = ";
         cin>>animalNou->grMancareZi;
+        cout<<"Rasa= ";
+        cin.get();
+        cin.getline(animalNou->rasa, 60, '\n');
         animalNou->urmator = NULL;
 
         adaugareAnimal(listaAnimale, animalNou);
 
-        padocDispobibil->numarAnimal = animalNou->numar;
-        padocDispobibil->ocupat = true;
+        nodProgramare *programareNoua = new nodProgramare;
+        programareNoua->numarAnimal = animalNou->numar;
+        programareNoua->dataInceput = *dataInceput;
+        programareNoua->dataSfarsit = *dataFinal;
+        programareNoua->fiuStanga = NULL;
+        programareNoua->fiuDreapta = NULL;
+
+        adaugareProgramarePadoc(padocDispobibil->programPadoc, programareNoua);// ii transmitem arborele nodului
     }
     else
-    {
         cout<<"Nu exista padocuri disbonibile! ";
-    }
 }
 
 void afisareListaAnimale(nodAnimal *lista)
@@ -137,30 +282,9 @@ void afisareListaAnimale(nodAnimal *lista)
 
     while(aux != NULL)
     {
-        cout<<"Animal " << aux->numar << ", nume " << aux->nume << ", inaltime " << aux->inaltime<< ", greutate " << aux->kg << ", mancare/zi " << aux->grMancareZi <<" gr"<<endl;
+        cout<<"Animal " << aux->numar << ", nume " << aux->nume << ", inaltime " << aux->inaltime<< ", greutate " << aux->kg << ", mancare/zi " << aux->grMancareZi <<" gr"<< ", rasa "<<aux->rasa<<endl;
         aux = aux->urmator;
     }
-}
-
-void eliberarePadoc(nodPadoc *lista)
-{
-    nodPadoc *aux = lista;
-    int numarPadoc;
-
-    cout<<"Numar padoc = ";
-    cin>>numarPadoc;
-
-    while(aux != NULL)
-    {
-        if(aux->numar == numarPadoc)
-        {
-            aux->ocupat = false;
-            aux->numarAnimal = -1;
-            break;
-        }
-        aux=aux->urmator;
-    }
-
 }
 
 int main()
@@ -170,7 +294,7 @@ int main()
 
     int i, comanda;
 
-    for(i = 0; i < 15; i++)
+    for(i = 1; i < 13; i++) // FOR PENTRU NUMAR PADOCURI!
         adaugarePadoc(listaPadocuri, i);
 
     while(true)
@@ -179,8 +303,7 @@ int main()
         cout<<"1 - Afisare lista padocuri"<<endl;
         cout<<"2 - Afisare lista animale"<<endl;
         cout<<"3 - Adaugare animal nou"<<endl;
-        cout<<"4 - Eliberare padoc"<<endl;
-        cout<<"5 - Inchidere program"<<endl;
+        cout<<"4 - Inchidere program"<<endl;
         cout<<"Comanda = ";
         cin>>comanda;
 
@@ -191,8 +314,6 @@ int main()
         else if(comanda == 3)
             citireAnimalNou(listaAnimale, listaPadocuri);
         else if(comanda == 4)
-            eliberarePadoc(listaPadocuri);
-        else if(comanda == 5)
             break;
     }
 
